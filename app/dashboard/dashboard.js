e8b70409c81f923623eebdcc5bde6dead66831df
gabriel <gabriel@gabriel>
2017-02-05 15:26:52 +0100

    clean seeder

---
+++ app/dashboard/dashboard.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.dashboard', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/dashboard', {
+    templateUrl: 'dashboard/dashboard.html',
+    controller: 'DashboardCtrl'
+  });
+}])
+
+.controller('DashboardCtrl', [function() {
+  $scope.user = {
+    "displayName":"arkangel",
+    "firstName":"gabriel",
+    "lastName":"vieira",
+    "email":"gabriel93250@gmail.com",
+    "picture":"https://api.deezer.com/user/22249217/image?size=medium"
+  }
+}]);
